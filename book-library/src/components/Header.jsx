47ec95ee847144be7cc95fd50a44f8c950453a36
Jugu Tenerat <133909875+tennjugu@users.noreply.github.com>
2024-01-21 20:13:04 +0100

    create header and add styles

--- book-library/src/components/Header.jsx
+++ book-library/src/components/Header.jsx
@@ -1,17 +1,13 @@
+import Logo from "./Logo"
+import SearchBar from "./SearchBar"
+import Signup from "./Signup-Login"
+
 const Header = () => {
     return (
         <div className="header">
-            <div className='logo'>
-                <img src="../src/assets/book.svg" alt="logo" />
-                <h1 className='logoText'>Tenn&apos;s Library</h1>
-            </div>
-            <div className='searchBar'>
-                <input type="text" id='search' placeholder='search a book'/>
-                <label htmlFor="search">Search Area</label>
-                <button className="searchButton">
-                    <img src="../src/assets/search.svg" alt="searchButton" />
-                </button>
-            </div>
+            <Logo />
+            <SearchBar />
+            <Signup />
         </div>
     )
 }
---
+++ book-library/src/components/Signup-Login.jsx
@@ -0,0 +1,16 @@
+const Signup = () => {
+    return (
+        <div className="signup-section">
+            <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            <div className="signup">
+                <a href="">Wish List</a>
+                <span></span>
+                <a href="">Log in</a>
+                <span></span>
+                <a href="">Sign Up</a>
+            </div>
+        </div>
+    )
+}
+
+export default Signup
