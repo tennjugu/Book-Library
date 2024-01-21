9ce2ae386cf4cfdd950c4c1e434d15a15c93d22c
Jugu Tenerat <133909875+tennjugu@users.noreply.github.com>
2024-01-21 20:34:37 +0100

    create navigation links, add styling

--- book-library/src/components/Header.jsx
+++ book-library/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import Logo from "./Logo"
 import SearchBar from "./SearchBar"
 import Signup from "./Signup-Login"
+import Navigation from "./Navigation"
 
 const Header = () => {
     return (
-        <div className="header">
-            <Logo />
-            <SearchBar />
-            <Signup />
+        <div className="main-head">
+            <div className="header">
+                <Logo />
+                <SearchBar />
+                <Signup />
+            </div>
+            <Navigation />
         </div>
     )
 }
